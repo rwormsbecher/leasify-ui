350bb0dc364660a06d1057c685e9a21db2d20acf
Rodney Wormsbecher <rodney22@live.nl>
2023-02-07 21:33:09 +0100

    add unit tests with jest to the library

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,10 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import packageJson from "./package.json" assert { type: "json" };
 
+import postcss from "rollup-plugin-postcss";
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
 export default [
     {
         input: "src/index.ts",
@@ -19,11 +23,19 @@ export default [
                 sourcemap: true,
             },
         ],
-        plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: "./tsconfig.json" }),
+            postcss(),
+            terser(),
+        ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
+        external: [/\.css$/],
     },
 ];
